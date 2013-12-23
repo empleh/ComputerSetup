7678c2110f276eea2a4faea80bfb220cf1b7e3c3
empleh <christian.peters@gmail.com>
2013-12-23 09:44:48 -0600

    Create RemoveTempFiles.bat

---
+++ RemoveTempFiles.bat
@@ -0,0 +1,15 @@
+:@echo
+c:
+
+:del /s /q /F "C:\Windows\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files\*"
+cd "C:\Windows\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files\"
+del /s /q /F *.*
+for /d %%i in (*) do rmdir /s /q "%%i"
+
+
+:del /s /q /F "C:\Windows\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files\*"
+cd "C:\Windows\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files\"
+del /s /q /F *.*
+for /d %%i in (*) do rmdir /s /q "%%i"
+
+:pause;
